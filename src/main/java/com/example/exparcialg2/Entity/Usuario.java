dbb9c5111bfa374b84450e4e881808baa0399543
lewis-coder <57689615+lewis-coder@users.noreply.github.com>
2020-06-06 22:26:55 -0500

    hibernate dependency

--- src/main/java/com/example/exparcialg2/Entity/Usuario.java
+++ src/main/java/com/example/exparcialg2/Entity/Usuario.java
@@ -1,15 +1,16 @@
 package com.example.exparcialg2.Entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
 
 @Entity
 public class Usuario {
     @Id
     @Column(name = "idUsuario")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idusuario;
+    @NotBlank
     private String nombre;
     private String apellido;
     private String dni;
