d69582554a6f80ded0d07a4ccf0b1858e99a6e77
Eduardo Sanchez <a20122509@pucp.pe>
2020-06-07 15:40:01 -0500

    sextocommit

--- src/main/java/com/example/exparcialg2/Config/Usuario2Controller.java
+++ src/main/java/com/example/exparcialg2/Config/Usuario2Controller.java
@@ -10,9 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,12 +32,24 @@ public class Usuario2Controller {
 
         Usuario usuarioLogueado = (Usuario) session.getAttribute("usuario");
         List<Pedido> listaPedidos= pedidoRepository.listaPedidos(usuarioLogueado.getIdusuario());
-        //for(Pedido p : listaPedidos){
-          //  p.getListaProductos();
+
+
+        //ist<Producto> listaProductosTotal= new ArrayList<Producto>();
+        //for(Pedido pe : listaPedidos){
+          //  for(Producto pro:pe.getListaProductos()){
+            //    listaProductosTotal.add(pro);
+            //}
         //}
+        //model.addAttribute("listaProductos",listaProductosTotal);
         model.addAttribute("listaPedidos",listaPedidos);
         return "pedidos/lista";
     }
+    @PostMapping("/buscar")
+    public String listabuscada(){
+        return "pedidos/lista";
+    }
+
+
 
 
 }
