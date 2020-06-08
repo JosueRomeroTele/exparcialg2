da7e1653a74d7800166abba01b49efbca13ed1ea
lewis-coder <57689615+lewis-coder@users.noreply.github.com>
2020-06-08 00:01:10 -0500

    precompra

--- src/main/java/com/example/exparcialg2/Config/ProductoController.java
+++ src/main/java/com/example/exparcialg2/Config/ProductoController.java
@@ -16,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -168,6 +169,10 @@ public class ProductoController {
     @GetMapping("/datos")
     public String datosProductos(Model model){
         return "producto/datos";
+    }
+
+    public String precompra(HttpSession session){
+        return "";
     }
 
 }
